67af7de9867c2d8e04d3e0c32b9a2086bf63c7a2
tru13014 <35413302+tru13014@users.noreply.github.com>
2018-02-17 20:27:00 -0800

    pre-submission
    Added this files for Sister Campbell to be able to see what I've done.

---
+++ assignments/franklin-6/scripts/franklin-6.js
@@ -0,0 +1,17 @@
+//<script type="text/javascript">
+//           var months = ['January','February','March','April','May','June','July',
+//'August','September','October','November','December'];       
+//var tomorrow = new Date();
+//tomorrow.setTime(tomorrow.getTime() + (1000*3600*24));       
+//document.getElementById("spanDate").innerHTML = months[tomorrow.getMonth()] + " " + tomorrow.getDate()+ ", " + tomorrow.getFullYear();
+//    </script>
+
+$(document).ready(function() {
+    
+    $(".burger-nav").on("click", function (){
+        
+        $("header nav ul").toggleClass("open")
+        
+    });
+    
+});
